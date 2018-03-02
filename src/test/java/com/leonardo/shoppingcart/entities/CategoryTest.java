5f9a997a14b12f53c05e1eb6e44f4f5adeeee36c
Leonardo <leon.mass.music@gmail.com>
2018-03-02 02:29:47 -0300

    Tests place holder creation

---
+++ src/test/java/com/leonardo/shoppingcart/entities/CategoryTest.java
@@ -0,0 +1,56 @@
+package com.leonardo.shoppingcart.entities;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class CategoryTest {
+
+    @Test
+    public void getId() {
+    }
+
+    @Test
+    public void getName() {
+    }
+
+    @Test
+    public void getDescription() {
+    }
+
+    @Test
+    public void getDisplayOrder() {
+    }
+
+    @Test
+    public void isDisabled() {
+    }
+
+    @Test
+    public void getProducts() {
+    }
+
+    @Test
+    public void setId() {
+    }
+
+    @Test
+    public void setName() {
+    }
+
+    @Test
+    public void setDescription() {
+    }
+
+    @Test
+    public void setDisplayOrder() {
+    }
+
+    @Test
+    public void setDisabled() {
+    }
+
+    @Test
+    public void setProducts() {
+    }
+}
---
+++ src/test/java/com/leonardo/shoppingcart/entities/CustomerTest.java
@@ -0,0 +1,48 @@
+package com.leonardo.shoppingcart.entities;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class CustomerTest {
+
+    @Test
+    public void getId() {
+    }
+
+    @Test
+    public void getFirstName() {
+    }
+
+    @Test
+    public void getLastName() {
+    }
+
+    @Test
+    public void getEmail() {
+    }
+
+    @Test
+    public void getPassword() {
+    }
+
+    @Test
+    public void setId() {
+    }
+
+    @Test
+    public void setFirstName() {
+    }
+
+    @Test
+    public void setLastName() {
+    }
+
+    @Test
+    public void setEmail() {
+    }
+
+    @Test
+    public void setPassword() {
+    }
+}
